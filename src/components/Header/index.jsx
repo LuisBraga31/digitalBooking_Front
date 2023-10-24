3741f974f7dfe5d0e14b592ef4e7c4f960214a21
Deborah-Borges <pretinha.deb@gmail.com>
2023-10-24 20:24:23 -0300

    Iniciando responsividade

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,31 @@
+import { IoMenu } from "react-icons/io5";
 import { Link } from 'react-router-dom';
+
 
 import DigitalBookingLogo from '../../assets/logo/db.svg';
 
 import styles from './Header.module.css';
 
 export default function Header() {
+
+
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-               <Link to='/'> <img src={DigitalBookingLogo} alt="" /> </Link> 
+               <Link to='/'> <div className={styles.image}> </div> </Link> 
             </div>
 
             <div className={styles.loginBotoes}>
                 <button className={styles.button}> Criar Conta </button>
                 <button className={styles.button}> Iniciar Sessão </button>
             </div>
+
+            <div className={styles.menuHamburger}>
+                <IoMenu className={styles.menu} size={28}/>
+            </div>
+
+
         </header>
     )
 
