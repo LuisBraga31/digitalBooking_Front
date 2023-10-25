0857b1e98f9b33e193fca725e586456a5e1d8fe2
Deborah-Borges <pretinha.deb@gmail.com>
2023-10-24 21:16:28 -0300

    Implementando Menu lateral

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,16 @@
 import { IoMenu } from "react-icons/io5";
+import { GrClose } from "react-icons/gr";
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-
-
-import DigitalBookingLogo from '../../assets/logo/db.svg';
+import { useState } from "react";
 
 import styles from './Header.module.css';
 
 export default function Header() {
 
+    const [menuLateral, setMenuLateral] = useState(false);
 
+    const showMenu = () => setMenuLateral(!menuLateral);
 
     return (
         <header className={styles.header}>
@@ -22,9 +24,40 @@ export default function Header() {
             </div>
 
             <div className={styles.menuHamburger}>
-                <IoMenu className={styles.menu} size={28}/>
+                <IoMenu className={styles.menu} size={28} onClick={showMenu}/>
             </div>
 
+            <nav className={ menuLateral ? styles.ativado : styles.desativado}>
+                <div className={styles.menuHeader}>
+                    <GrClose className={styles.closeButton} size={22} onClick={showMenu}/>
+                    <h4 className={styles.menuTitle}> Menu </h4>
+                </div>
+                <div className={styles.menuBody}>
+                    <div className={styles.menuButtons}>
+                        <button className={styles.buttonItem}> Criar Conta </button>
+                        <hr color="black" width="90%" size="1" className={styles.linha}/>
+                        <button className={styles.buttonItem}> Iniciar Sessão </button>
+                    </div>
+                    <div>
+                        <ul className={styles.lista}>
+                            <li>
+                                <FaFacebook size={24}/>
+                            </li>
+                            <li>
+                                <FaInstagram size={24}/>
+                            </li>
+                            <li>
+                                <FaLinkedin size={24}/>
+                            </li>
+                            <li>
+                                <FaTwitter size={24}/>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>   
+
+            </nav>
 
         </header>
     )
