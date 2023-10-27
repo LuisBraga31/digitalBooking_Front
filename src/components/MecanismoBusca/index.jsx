45b6f41260beb48c40519d7432e2846aa4511adf
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-27 16:33:01 -0300

    Finalizando mecanismo de busca

--- src/components/MecanismoBusca/index.jsx
+++ src/components/MecanismoBusca/index.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import styles from './Mecanismo.module.css';
 
 export default function MecanismoBusca() {
+
+  const [selectedValue, setSelectedValue] = useState('')
+
+  const selectChange = (event) => {
+    setSelectedValue(event.target.value); 
+  };
+
   return (
     <div className={styles.mecanismo}>
         <h1> Buscar ofertas em hotéis, casas e muito mais </h1>
         
         <form className={styles.formBusca}>
-            <input className={styles.buscaInput} type="text" placeholder="Onde Vamos?"/>
+            <select value={selectedValue} onChange={selectChange} className={styles.buscaSelect} type="text">
+              <option value="" disabled hidden> Onde Vamos ?</option>
+              <option value="opcao1"> Rio de Janeiro <span className={styles.customOption}> Brasil </span> </option>
+              <option value="opcao2"> Porto de Galinhas - Brasil </option>
+              <option value="opcao3"> Santos - Brasil </option>
+            </select>
             <input className={styles.buscaInput} type="text" placeholder="Check in - Check out"/>
             <button className={styles.buscaButton} type="submit"> Buscar </button>
         </form>
