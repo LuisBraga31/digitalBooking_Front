0efbe40224c56b0d6fb9479db2f81823a89437c2
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-26 21:32:51 -0300

    Corrigindo pequenos bugs

--- src/components/LoginForm/Index.jsx
+++ src/components/LoginForm/Index.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 
 import styles from "./LoginForm.module.css";
 
-
-
 export default function LoginForm() {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
