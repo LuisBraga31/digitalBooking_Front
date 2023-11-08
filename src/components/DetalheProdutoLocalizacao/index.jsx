f23029e7b597cd02b6fd61a86ca5c589a6baa10e
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-08 13:36:13 -0300

    Finalizando componente de localização

--- src/components/DetalheProdutoLocalizacao/index.jsx
+++ src/components/DetalheProdutoLocalizacao/index.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable react/prop-types */
 import styles from './DetalheProdutoLocalizacao.module.css';
 
-export function DetalheProdutoLocalizacao( {produto}) {
-    return(
-        
-        <div className={styles.local}>
-            <h1> Onde você vai estar? </h1>
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-            <p> {produto.location} </p>
+export function DetalheProdutoLocalizacao( {produto} ) {
+    return (
+      <div className={styles.containerLocal}>
+        <h1 className={styles.title}> Onde você vai estar? </h1>
 
-            <div>
-                
-            </div>
-        
+        <p className={styles.locationTitle}> {produto.location} </p>
+
+        <div className={styles.mapArea}>
+          <MapContainer className={styles.map} center={[-22.9031, -43.255]} zoom={15}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[-22.9031, -43.255]}>
+              <Popup> Sua Localização </Popup>
+            </Marker>
+          </MapContainer>
         </div>
-    )
+      </div>
+    );
 }
