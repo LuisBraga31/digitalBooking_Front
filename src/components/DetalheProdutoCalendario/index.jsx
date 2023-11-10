baa0eedd7ef3dd5448d328c91cb923bf4d760b2b
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-09 21:14:20 -0300

    Criando o componente de calendario

--- src/components/DetalheProdutoCalendario/index.jsx
+++ src/components/DetalheProdutoCalendario/index.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from './DetalheProdutoCalendario.module.css';
+import './calendario.css';
+
 export function DetalheProdutoCalendario() {
-    return(
-        <div>
-            
+    
+    const [activeStartDate, setActiveStartDate] = useState(new Date(2023, 10, 9));
+
+    const handleDateChange = (date) => {
+      setActiveStartDate(date);
+    };
+    
+    const desabilitarDatas = ({ date }) => {
+        const dataAtual = new Date();
+        const dataAnterior = date < dataAtual;
+        
+        return dataAnterior;
+    }
+    
+    
+    return (
+      <div className={styles.containerCalendario}>
+        <h1 className={styles.dataTitle}> Datas Disponíveis</h1>
+
+        <div className={styles.sectionReserva}>
+          <div className={styles.calendario}>
+            <Calendar
+              value={activeStartDate}
+              onChange={handleDateChange}
+              showDoubleView
+              tileDisabled={desabilitarDatas}
+              showNavigation={true}
+            />
+          </div>
+
+          <div className={styles.reserva}>
+            <p>Adicione as datas da sia viagem para obter preços exatos.</p>
+            <button>Iniciar reserva</button>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
