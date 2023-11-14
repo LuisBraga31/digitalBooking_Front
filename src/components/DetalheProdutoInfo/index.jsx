3f5ebd92eb080916690b9438b2187679a83cc930
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-13 21:21:16 -0300

    Ajustes no detalhe produto info

--- src/components/DetalheProdutoInfo/index.jsx
+++ src/components/DetalheProdutoInfo/index.jsx
@@ -10,21 +10,21 @@ export function DetalheProdutoInfo() {
             <div className={styles.bloco}>
                 
                 <div className={styles.blocoInfo}>
-                    <h3 className={styles.subtituloInfo}>Regras da casa</h3>
+                    <h3>Regras da casa</h3>
                     <p>Check-out: 10:00</p>
                     <p>Não é permitido festas</p>
                     <p>Não fumar</p>
                 </div>
 
                 <div className={styles.blocoInfo}>
-                    <h3 className={styles.subtitulo}>Saúde e segurança</h3>
+                    <h3 >Saúde e segurança</h3>
                     <p>Diretrizes de distanciamento social e outras regulamentações relacionadas ao coronavírus se aplicam</p>
                     <p>Detector de fumaça</p>
                     <p>Depósito de segurança</p>
                 </div>
 
                 <div className={styles.blocoInfo}>
-                    <h3 className={styles.subtitulo}>Política de cancelamento</h3>
+                    <h3 >Política de cancelamento</h3>
                     <p>Adicione as datas da viagem para obter detalhes de cancelamento para esta estadia.</p>
                 </div>
 
