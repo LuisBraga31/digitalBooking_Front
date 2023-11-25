129a9f4651d51c36854439746de70d0b2da9b2a8
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-24 23:40:12 -0300

    Ajustes darkmode e criando primeira validação de reserva

--- src/pages/ReservaPage/index.jsx
+++ src/pages/ReservaPage/index.jsx
@@ -5,15 +5,39 @@ import styles from './ReservaPage.module.css';
 import elementos from '../../data/elements.json';
 
 import { useState, useContext, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { TemaContext } from "../../contexts/globalContext";
 import { Reserva } from "../../components/Reserva";
+
+import Swal from 'sweetalert2';
 
 export function ReservaPage() {
 
     const { tema } = useContext(TemaContext);
     const [produto, setProduto] = useState([]);
     const produtoId = useParams();
+    const navigate = useNavigate();
+
+    const estaLogado = !!localStorage.getItem("usuarioLogado");
+
+    const verificarLogin = () => {
+        if(!estaLogado) {
+            navigate('/login');
+            Swal.fire({
+                icon: 'error',
+                title: 'Atenção:',
+                text: "Faça login na aplicação para acessar página de reserva",
+                confirmButtonColor: '#1DBEB4',
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login');
+                } else {
+                    navigate('/login');
+                }
+            })
+            
+        }
+    }
     
     const getProduto = () => {
         const produtoEncontrado = elementos.find(item => item.id === parseInt(produtoId.id));
@@ -22,13 +46,22 @@ export function ReservaPage() {
 
     useEffect(() => {
         getProduto();
+        verificarLogin();
+    }, []);
+
+    useEffect(() => {
+        window.scrollTo({
+          top: 0,
+          transitionDelay: 300,
+          behavior: "smooth"
+        });
     }, []);
 
     return (
         <>
             <Header />
             <main className={`${styles.main} ${tema ? '' : styles.darkMode}`}>
-                <Reserva produto={produto}/>
+                <Reserva produto={produto}/>       
             </main>
             <Footer />
         </>
