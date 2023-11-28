5682d80e106766028be0d86634d9f57ab7e91a06
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-28 14:40:48 -0300

    Configurando exibição de calendario no mecanismo de busca

--- src/components/MecanismoBusca/index.jsx
+++ src/components/MecanismoBusca/index.jsx
@@ -4,13 +4,40 @@ import { TemaContext } from "../../contexts/globalContext";
 import styles from './Mecanismo.module.css';
 
 import { api } from '../../services/api';
+import { Modal } from '../Modal';
+
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function MecanismoBusca( {setFilterLocation} ) {
 
   const { tema } = useContext(TemaContext);
 
+  const [openModal, setOpenModal] = useState(false);
+  const [dataInicio, setDataInicio] = useState('');
+  const [dataFinal, setDataFinal] = useState('');
+  const [meses, setMeses] = useState([]);
   const [selectedValue, setSelectedValue] = useState('');
   const [listaCidades, setListaCidades] = useState([{'id': 1, 'nome': 'Santos-SP'}, {'id': 2, 'nome': 'São Paulo-SP'}, {'id': 3, 'nome': 'Rio de Janeiro-RJ'}]);
+
+  const desabilitarDatas = ({ date }) => {
+    const dataAtual = new Date();
+    const dataAnterior = date < dataAtual;
+    return dataAnterior;
+  }
+
+  const handleDateChange = (date) => {
+      const meses = [];
+      const converteMes = { month: 'long' };
+      const mesInicio = date[0].toLocaleString('pt-BR', converteMes);
+      const mesFinal = date[1].toLocaleString('pt-BR', converteMes);
+      meses.push(mesInicio);
+      meses.push(mesFinal);
+      
+      setMeses(meses);
+      setDataInicio(date[0]);
+      setDataFinal(date[1]);
+  };
 
   const selectChange = (event) => {
     setSelectedValue(event.target.value); 
@@ -19,6 +46,7 @@ export default function MecanismoBusca( {setFilterLocation} ) {
   const handleSearchForm = (e) => {
     e.preventDefault();
     setFilterLocation(selectedValue);
+    console.log(selectedValue)
   }
 
   // const getListaCidades = async() => {
@@ -43,9 +71,33 @@ export default function MecanismoBusca( {setFilterLocation} ) {
               ))}
 
             </select>
-            <input className={styles.buscaInput} type="text" placeholder="Check in - Check out"/>
+            <span className={styles.buscaDatas} onClick={() => setOpenModal(true)}>
+              {dataInicio ? `${dataInicio.getDate()} de ${meses[0].slice(0, 3)}. ~ ${dataFinal.getDate()} de ${meses[1].slice(0, 3)}.` : 'Check in - Check out'}
+            </span>
             <button className={styles.buscaButton} type="submit"> Buscar </button>
         </form>
+
+        <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+          <div className={styles.calendarioForm}>
+            <span className={styles.modalCalendarTitle}> Selecione sua data de reserva </span>
+              <Calendar
+                className={styles.calendarModal}
+                onChange={handleDateChange}
+                showDoubleView
+                tileDisabled={desabilitarDatas}
+                showNavigation={true}
+                selectRange={true}
+              />
+              <Calendar
+                className={styles.calendarModalMobile}
+                onChange={handleDateChange}
+                tileDisabled={desabilitarDatas}
+                showNavigation={true}
+                selectRange={true}
+              />
+              <button className={styles.okButton} onClick={() => setOpenModal(false)}> Ok </button>
+          </div>
+        </Modal>
     
     </div>
   )
