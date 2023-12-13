2aaf3c926f5f29640e1fef016a98158ff6d36c58
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-12-12 23:21:32 -0300

    Adicionando validações ao formulario de produto

--- src/components/FormAdmin/index.jsx
+++ src/components/FormAdmin/index.jsx
@@ -18,6 +18,7 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
     const [listaImagens, setListaImagens] = useState([]);
     const [listaAtributos, setListaAtributos] = useState([]);
 
+    const [formErrors, setFormErrors] = useState({});
     const [formData, setFormData] = useState({
         nome: '',
         descricao:'',
@@ -28,7 +29,7 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
     const estaLogado = !!localStorage.getItem("token");
     const token = estaLogado ? localStorage.getItem("token") : null;
 
-    const [atributoQtd, setAtributoQtd] = useState(1);
+    const [atributoQtd, setAtributoQtd] = useState(2);
     const [imagemQtd, setImagemQtd] = useState(5);
     const atributos = new Array(atributoQtd).fill(null);
     const imagens = new Array(imagemQtd).fill(null);
@@ -51,15 +52,24 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
     }
     
     const handleChangeCategory = (event) => {
-        setSelectedCategory(event.target.value); 
+        setSelectedCategory(event.target.value);
+        if (formErrors[event.target.name]) {
+            setFormErrors({ ...formErrors, [event.target.name]: '' });
+        } 
     };
 
     const handleChangeCity = (event) => {
-        setSelectedCity(event.target.value); 
+        setSelectedCity(event.target.value);
+        if (formErrors[event.target.name]) {
+            setFormErrors({ ...formErrors, [event.target.name]: '' });
+        } 
     };
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
+        if (formErrors[e.target.name]) {
+            setFormErrors({ ...formErrors, [e.target.name]: '' });
+        }
     }
 
     const handleImagensChange = (e, index) => {
@@ -69,6 +79,12 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
           novaLista[index] = { titulo: `Imagem ${index + 1}`, url: value };
           return novaLista;
         });
+        if (formErrors['imagens']) {
+            setFormErrors({ ...formErrors, ['imagens']: '' });
+        }
+        if (formErrors['imagensVazio']) {
+            setFormErrors({ ...formErrors, ['imagensVazio']: '' });
+        } 
     };
 
     const handleAtributosChange = (e, index, campo) => {
@@ -78,52 +94,68 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
             novaLista[index] = { ...novaLista[index], [campo]: value };
             return novaLista;
         });
+        if (formErrors['atributos']) {
+            setFormErrors({ ...formErrors, ['atributos']: '' });
+        }
+        if (formErrors['nomeAtributoVazio']) {
+            setFormErrors({ ...formErrors, ['nomeAtributoVazio']: '' });
+        }
+        if (formErrors['urlAtributoVazio']) {
+            setFormErrors({ ...formErrors, ['urlAtributoVazio']: '' });
+        }    
     };
 
     const handleForm = async (e) => {
         e.preventDefault();
-    
-        try {
-            const caracteristicasPromises = listaAtributos.map(async (atributo) => {
-                const response = await api.post('/v1/caracteristicas', atributo, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    },
+        
+        if(validForm()) {
+            try {
+                const caracteristicasPromises = listaAtributos.map(async (atributo) => {
+                    const response = await api.post('/v1/caracteristicas', atributo, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`,
+                        },
+                    });
+                    return response.data.id;
                 });
-                return response.data.id;
-            });
-    
-            const listaAtributosIds = await Promise.all(caracteristicasPromises);
-    
-            const imagensPromises = listaImagens.map(async (imagem) => {
-                const response = await api.post('/v1/imagens', imagem, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    },
+        
+                const listaAtributosIds = await Promise.all(caracteristicasPromises);
+        
+                const imagensPromises = listaImagens.map(async (imagem) => {
+                    const response = await api.post('/v1/imagens', imagem, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`,
+                        },
+                    });
+                    return response.data.id;
                 });
-                return response.data.id;
-            });
-
-            const listaImagensIds = await Promise.all(imagensPromises);
-            
-            const formProdutoAdmin = {
-                nome: formData.nome,
-                descricao: formData.descricao,
-                latitude: formData.latitude,
-                longitude: formData.longitude,
-                cidadesId: selectedCity,
-                categoriasId: selectedCategory,
-                imagensId: listaImagensIds,
-                caracteristicasProdutoId: listaAtributosIds,
-            };
     
-            await postProduto(formProdutoAdmin);
-    
-        } catch (error) {
-            console.log(error);
+                const listaImagensIds = await Promise.all(imagensPromises);
+                
+                const formProdutoAdmin = {
+                    nome: formData.nome,
+                    descricao: formData.descricao,
+                    latitude: formData.latitude,
+                    longitude: formData.longitude,
+                    cidadesId: selectedCity,
+                    categoriasId: selectedCategory,
+                    imagensId: listaImagensIds,
+                    caracteristicasProdutoId: listaAtributosIds,
+                };
+        
+                await postProduto(formProdutoAdmin);
+        
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.log('error')
+            return null;
         }
+
+        
     };
 
     const postProduto = async (formProdutoAdmin) => {
@@ -158,6 +190,56 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
         }
     };
 
+    const validForm = () => {
+        let errors = {};
+
+        if(formData.nome.trim() === '') {
+         errors.nome='O nome é obrigatório!'; 
+        }
+        if(formData.descricao.length <= 10) {
+          errors.descricao='A descrição precisa de no mínimo 10 caracteres'; 
+        }
+        if(formData.longitude.trim() === '') {
+          errors.longitude='A longitude é obrigatória'; 
+        }
+        if(formData.latitude.trim() === '') {
+          errors.latitude='A latitude é obrigatória'; 
+        }
+
+        if(listaImagens.length < 5) {
+            errors.imagens='É necessário no mínimo 5 imagens';
+        }
+        for(let i = 0; i < listaImagens.length; i++) {
+            if((listaImagens[i].url).length === 0) {
+                errors.imagensVazio='Uma Url está vazia';
+            }
+        }
+
+        if(listaAtributos.length < 2) {
+            errors.atributos='É necessário no mínimo 2 atributos';
+        }
+        for(let i = 0; i < listaAtributos.length; i++) {
+
+            if(listaAtributos[i].nome.length === 0) {
+                errors.nomeAtributoVazio='Há nomes de atributos vazios';
+            }
+            if(listaAtributos[i].icone.length === 0) {
+                errors.urlAtributoVazio='Há urls de atributos vazios';
+            }
+        }
+
+        if(selectedCity === 'Default') {
+            errors.cidade = 'Selecione uma cidade';
+        }
+        if(selectedCategory === 'Default') {
+            errors.categoria = 'Selecione uma categoria';
+        }
+
+        setFormErrors(errors);
+        return Object.keys(errors).length === 0;
+        
+    }
+
     return (
         <>
             
@@ -178,53 +260,54 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
 
                 <form className={styles.form} onSubmit={handleForm}>
                     <div className={styles.sectionGrid}>
-                        <div className={styles.input}>
+                        <div className={formErrors.nome ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                             <label htmlFor="">Nome da Propriedade</label>
-                            <input name='nome' type="text" placeholder='Nome' onChange={handleChange} required/>
+                            <input name='nome' type="text" placeholder='Nome' onChange={handleChange}/>
+                            {formErrors.nome && <span>{formErrors.nome}</span>}
                         </div>
 
-                        <div className={styles.input}>
+                        <div className={formErrors.categoria ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                             <label htmlFor="">Categoria</label>
-                            <select value={selectedCategory} onChange={handleChangeCategory} type="text" required> 
+                            <select name="categoria" value={selectedCategory} onChange={handleChangeCategory} type="text" required> 
                                 <option value="Default" disabled hidden> Categorias </option>
                                 {listaCategorias.map(item => (
                                     <option key={item.id} value={item.id}> {item.nome} </option>
                                 ))}
                             </select>
+                            {formErrors.categoria && <span>{formErrors.categoria}</span>}
                         </div>
 
-                        <div className={styles.input}>
-                            <label htmlFor="">Endereço</label>
-                            <input type="text" placeholder='Endereço' required/>
-                        </div>
-
-                        <div className={styles.input}>
+                        <div className={formErrors.cidade ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                             <label htmlFor="">Cidade</label>
-                            <select value={selectedCity} onChange={handleChangeCity} type="text" required> 
+                            <select name="cidade" value={selectedCity} onChange={handleChangeCity} type="text" required> 
                                 <option value="Default" disabled hidden> Cidades </option>
                                 {listaCidades.map(item => (
                                     <option key={item.id} value={item.id}> {item.nome} </option>
                                 ))}
                             </select>
+                            {formErrors.cidade && <span>{formErrors.cidade}</span>}
                         </div>
 
-                        <div className={styles.input}>
+                        <div className={formErrors.latitude ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                             <label htmlFor="">Latitude</label>
-                            <input name='latitude' type="text" placeholder='Latitude' onChange={handleChange} required/>
+                            <input name='latitude' type="text" placeholder='Latitude' onChange={handleChange}/>
+                            {formErrors.latitude && <span>{formErrors.latitude}</span>}
                         </div>
 
-                        <div className={styles.input}>
+                        <div className={formErrors.longitude ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                             <label htmlFor="">Longitude</label>
-                            <input name='longitude' type="text" placeholder='Longitude' onChange={handleChange} required/>
+                            <input name='longitude' type="text" placeholder='Longitude' onChange={handleChange} />
+                            {formErrors.longitude && <span>{formErrors.longitude}</span>}
                         </div>
                     </div>
 
-                    <div className={styles.descricao}>
+                    <div className={formErrors.descricao ? `${styles.input} ${styles.error}` : `${styles.input}`}>
                         <label htmlFor="">Descrição</label>
-                        <textarea name="descricao" id="" cols="30" rows="10" placeholder='Descrição' onChange={handleChange} required></textarea>
+                        <textarea name="descricao" id="" cols="30" rows="10" placeholder='Descrição' onChange={handleChange} ></textarea>
+                        {formErrors.descricao && <span>{formErrors.descricao}</span>}
                     </div>
 
-                    <div className={styles.atributos}>
+                    <div className={formErrors.atributos || formErrors.nomeAtributoVazio || formErrors.urlAtributoVazio ? `${styles.atributos} ${styles.error}` : `${styles.atributos}`}>
                         <h2>Adicionar Atributos</h2>
                         
                         {atributos.map((_, index) => ( 
@@ -247,7 +330,9 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
                         </div>
                         
                         ))}                        
-                        
+                        {formErrors.atributos && <span>{formErrors.atributos}</span>}
+                        {formErrors.nomeAtributoVazio && <span>{formErrors.nomeAtributoVazio}</span>}
+                        {formErrors.urlAtributoVazio && <span>{formErrors.urlAtributoVazio}</span>}
                     </div>
 
                     <div className={styles.produto}>
@@ -271,20 +356,21 @@ export function FormAdmin( {listaCidades, listaCategorias} ) {
                         </div>
                     </div>
 
-                    <div className={styles.carregarImagens}>
+                    <div className={formErrors.imagens || formErrors.imagensVazio ? `${styles.carregarImagens} ${styles.error}` : `${styles.carregarImagens}`}>
 
                     <h3>Carregar imagens</h3>
 
                     {imagens.map((_, index) => (
                     
                         <div className={styles.imagensInput} key={index}>
-                            <input type="text" placeholder='Url da Imagem' onChange={(e) => handleImagensChange(e, index)}/>     
+                            <input type="url" placeholder='Url da Imagem' onChange={(e) => handleImagensChange(e, index)}/>     
                             {index == imagemQtd-1 && <button onClick={addImagens}>+</button>}
                             
                         </div>
                     
                     ))}
-
+                    {formErrors.imagens && <span>{formErrors.imagens}</span>}
+                    {formErrors.imagensVazio && <span>{formErrors.imagensVazio}</span>}
                     </div>
 
                     <div className={styles.btnCriar}>
